8a2f9114af04438a7c5f3ce686e0edf53e0ff36f
Adva Haetzni <adva16@meet.mit.edu>
2015-02-26 19:53:00 +0200

    i addad information to blocks on supermariobros

--- classes/blocks.py
+++ classes/blocks.py
@@ -1,4 +1,27 @@
 import turtle
 class blocks(turtle):
-def__init__(self,canvas,dx,dy,x,y,heigth,):
+	def __init__(self,canvas,dx,dy,x,y,height,width):
+		RawTurtle.__init__(self,canvas)
+		self.penup()
+		self.goto(x,y)
+		self.size = size
+		self.dx = dx
+		self.dy = dy
+		self.shape("squer"+str(size)
+
+	def getsize(self):
+	    return self.size
+
+	def getDX(self):
+	    return self.dx
+
+	def getDY(self):
+	    return self.dy
+	
+	def setDX(self,dx):
+	    self.dx = dx
+
+	def setDY(self,dy):
+	    self.dy = dy
+
 
