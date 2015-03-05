ec7ba2ec30ff2c2e310e0771ca9756535252228b
Adva Haetzni <adva16@meet.mit.edu>
2015-03-05 19:21:00 +0200

    i finished both classes:blocks and enemy

--- classes/ClassEnemy.py
+++ classes/ClassEnemy.py
@@ -1,19 +1,40 @@
 from turtle import *
 class Enemy(Turtle):
-	def __init__ (self, canvas, x, y, dx, dy, height, width, EnemyType):
+	def __init__ (self, canvas, x, y, dx, dy, height, width, ):
 		RawTurtle.__init__(self, canvas)
 		self.penup()
 		self.goto(x,y)
 		self.dx = dx
 		self.dy= dy
 		self.height = height
-		self.width = width
-		self.EnemyType = EnemyType
-		self.shape("square")
+		self.width = width	
+		self.shape("triangle")
+
+
+	def getsize(self):
+	    return self.size
+
+	def getDX(self):
+	    return self.dx
+
+	def getDY(self):
+	    return self.dy
+	
+	def setDX(self,dx):
+	    self.dx = dx
+
+	def setDY(self,dy):
+	    self.dy = dy	
+
+	def getwidth(self,width):
+		return self.width
+
+	def getheight(self,height):
+		return self.height	   
+
 	def move():
-		x += dx
-		y += dy
+		x = self.xcor()
+		y = self.ycor()
+		x += self.dx
+		y += self.dy
 		self.goto(x,y)
-
-
-screen.register_shape("assests/Goomba2.gif")
